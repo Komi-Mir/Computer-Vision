da3d998ddc082c49d2bf253cfc73a00b28111504
KomiMir <anastasiaperamakova@gmail.com>
2026-05-27 22:24:33 +0700

    end gauss

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 #define STB_IMAGE_IMPLEMENTATION
@@ -57,13 +58,74 @@ void median(Image_struct* img_f, Image_struct* img_r, int radius) {
         }
     }
 
+    free(window);
+
 
     
 }
 
 
 
-void gauss() { print("Фильтр Гаусса");}
+void gauss(Image_struct* img_f, Image_struct* img_r, int radius, float sigma) {
+    int w = img_f->width;
+    int h = img_f->height;
+    int c = img_f->channels;
+    int size_windows = 2 * radius + 1;
+
+    img_r->width = w;
+    img_r->height = h;
+    img_r->channels = c;
+    img_f->img = malloc(w * h * c);
+
+    unsigned char *window = malloc(size_windows);
+    // Ядра Гаусса
+    float *ker = malloc(size_windows * size_windows * sizeof(float));
+    float summ = 0.0f;
+    for (int ny = -radius; ny <= radius; ny++) {
+        for (int nx = -radius; nx <= radius; nx++) {
+            float a = expf(-(nx * nx + ny * ny) / (2.0f * sigma * sigma));
+            ker[(ny + radius) * size_windows + (nx + radius)] = a;
+            summ += a;
+
+            }
+    }
+    // нормализует сумма весов = 1
+    for (int i = 0; i < size_windows * size_windows; i++) {
+        ker[i] /= summ;
+    }
+
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            for (int chan = 0; chan < h; chan++) {
+                float res = 0.0f;
+
+                for (int ny = -radius; ny <= radius; ny++) {
+                    for (int nx = -radius; nx <= radius; nx++) {
+                        int oy = y + ny;
+                        int ox = x + nx;
+                        
+                        if (oy < 0) oy = -oy;
+                        if (ox < 0) ox = -ox;
+                        if (ox >= h) ox = 2 * h - ox - 2;
+                        if (ox >= h) ox = 2 * h - oy - 2;
+
+                        float weight = ker[(ny + radius) * size_windows + (nx + radius)];
+                        res += weight * img_f->img[(oy * w + ox) * c + chan];
+
+                    }
+                }
+                img_r->img[(y * w + x) * c + chan] = (unsigned char)res;
+                
+            }
+            
+
+        }
+    }
+
+    free(ker);
+
+
+}
 void edges() { print("Детекция границ");}
 void convolution() { print("Свертка");}
 
