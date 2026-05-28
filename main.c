91e24c65aa34bf6647c6cd0825a5c1dde6cce843
KomiMir <anastasiaperamakova@gmail.com>
2026-05-28 12:53:10 +0700

    end convolution

--- main.c
+++ main.c
@@ -182,7 +182,49 @@ void detector(Image_struct* img_f, Image_struct* img_r) {
         }
     }
 }
-void convolution() { print("Свертка");}
+void convolution(Image_struct* img_f, Image_struct* img_r, int radius, float *ker) {
+    int w = img_f->width;
+    int h = img_f->height;
+    int c = img_f->channels;
+    int size_windows = 2 * radius + 1;
+
+    img_r->width = w;
+    img_r->height = h;
+    img_r->channels = c;
+    img_f->img = malloc(w * h * c);
+
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            for (int chan = 0; chan < h; chan++) {
+                float res = 0.0f;
+
+                for (int ny = -radius; ny <= radius; ny++) {
+                    for (int nx = -radius; nx <= radius; nx++) {
+                        int oy = y + ny;
+                        int ox = x + nx;
+                        
+                        if (oy < 0) oy = -oy;
+                        if (ox < 0) ox = -ox;
+                        if (ox >= h) ox = 2 * h - ox - 2;
+                        if (ox >= h) ox = 2 * h - oy - 2;
+
+                        float weight = ker[(ny + radius) * size_windows + (nx + radius)];
+                        res += weight * img_f->img[(oy * w + ox) * c + chan];
+
+                    }
+                }
+
+                if (res < 0.0f) res = 0.0f;
+                if (res > 255.0f) res = 255.0f;
+                
+                img_r->img[(y * w + x) * c + chan] = (unsigned char)res;
+                
+            }
+            
+
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
     // Загразука изображения
